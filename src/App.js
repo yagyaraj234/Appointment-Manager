48aabff78b3525be85adb133ba46f0c2f111ee06
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-04 23:06:53 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { BiArchive } from "react-icons/bi";
+import "./App.css";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
 
 function App() {
   return (
-    <div className="App">
-     <h1 className='text-3xl '>Hello</h1>
+    <div className="">
+      <h1 className="bold text-3xl flex justify-center space m-5">
+        <BiArchive className="bold col-red-600 mr-5 mt-1" />
+        Your Appointments
+      </h1>
+      <AddAppointment></AddAppointment>
+      <Search></Search>
     </div>
   );
 }
